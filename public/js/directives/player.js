2b96b463ea5a89c495b42ddeaa97a2436bddcbff
Jeff Jones <jeff.jones1@gmail.com>
2015-09-16 16:45:36 -0400

    TS% up and running as custom scope attribute

--- public/js/directives/player.js
+++ public/js/directives/player.js
@@ -6,7 +6,9 @@
       templateUrl: "views/players/_player.html",
       link: function($scope){
         var player = $scope.player
-        $scope.eFG = (((Number(player.threePtMade) * 0.5  + Number(player.fgm))/ Number(player.fga)) * 100).toFixed(2);
+        $scope.eFG = (((Number(player.threePtMade) * 0.5  + Number(player.fgm))/ Number(player.fga)) * 100).toFixed(1);
+        $scope.TSA =  (0.44 * Number(player.FTA)) + Number(player.fga)
+        $scope.TS = ((Number(player.pts)/(2 * $scope.TSA)) * 100).toFixed(1)
       }
     };
   }
